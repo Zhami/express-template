9fa1f27d89642c8ec5b1a37ee7febf7a46f9ba6a
Tim <tim@fostle.com>
2011-05-28 20:46:35 -0700

    Routes tests

--- test/app/routes.test.js
+++ test/app/routes.test.js
@@ -1,3 +1,21 @@
 require('../common')
 
 var test = microtest.module('app/routes.js')
+
+test.requires('./common', { name: 'common' })
+
+var load = test.required.common.load = test.function('load')
+  , APP  = test.object('app')
+
+EXPORTS = test.compile()
+
+test.describe('test routes', function () {
+  var ROUTE = test.object('route')
+
+  test.expect(load, 1, ['home'], ROUTE)
+  test.expect(APP, 'get', 1, ['/', ROUTE])
+  test.expect(load, 1, ['home'], ROUTE)
+  test.expect(APP, 'get', 1, ['/home', ROUTE])
+
+  EXPORTS(APP)
+})
